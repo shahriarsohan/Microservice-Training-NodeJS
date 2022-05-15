d2ca49ccab66ee10db648da853e631b6bbb77e5b
shahriarsohan <sohanmock@gmail.com>
2022-05-15 12:51:49 +0600

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const PORT = 3000;
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must provided");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must provided");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Auth database connected to DB");
+  } catch (e) {
+    console.log("Error connection to auth database", e);
+  }
+
+  app.listen(PORT, () => {
+    console.log("Auth service listening on port", PORT);
+  });
+};
+
+start();
